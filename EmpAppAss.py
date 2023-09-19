5430f207e9c7b73a17fdbd420e4e238fba9e9b6c
mastemewbie <102252530+mastemewbie@users.noreply.github.com>
2023-09-20 02:03:38 +0800

    Update EmpAppAss.py

--- EmpAppAss.py
+++ EmpAppAss.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import  Flask, render_template, request, jsonify, redirect, url_for, Response
 from pymysql import connections
 import os
 import boto3
